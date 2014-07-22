34baa5ab4d8c78159796b531dea126d30b4a4b3d
Dennis Müller <dennis.briefkasten@gmail.com>
2014-07-22 02:21:06 +0200

    Added new Renamed Pocal Dir via cmd

---
+++ Pocal/ViewModels/HourListItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pocal.ViewModels
+{
+    public class HourListItem
+    {
+        public string time { get; set; }
+    }
+}
