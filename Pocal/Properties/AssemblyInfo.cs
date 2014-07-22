53f823e456bc1d4b3f827b589d9bb3d6848ab521
Dennis Müller <dennis.briefkasten@gmail.com>
2014-07-22 02:43:41 +0200

    Last Changes of Renaming

--- Pocal/Properties/AssemblyInfo.cs
+++ Pocal/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ListCalendar")]
+[assembly: AssemblyTitle("Pocal")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ListCalendar")]
+[assembly: AssemblyProduct("Pocal")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
