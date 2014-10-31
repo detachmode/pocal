2270fd2a731959d5a01ac7d4109f77dd4f29b09b
Dennis <dennis.briefkasten@gmail.com>
2014-10-31 18:02:22 +0100

    Bugfix: Agenda Scrolls While SingleDayView is open

--- Pocal/MainPage.xaml.cs
+++ Pocal/MainPage.xaml.cs
@@ -13,203 +13,213 @@ using System.Threading.Tasks;
 
 namespace Pocal
 {
-	public partial class MainPage : PhoneApplicationPage
-	{
-		private bool apptWasTapped = false;
-		private bool firstTimeOpenSingleDayview = true;
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private bool apptWasTapped = false;
+        private bool firstTimeOpenSingleDayview = true;
 
-		// Constructor
-		public MainPage()
-		{
-			InitializeComponent();
-			DataContext = App.ViewModel;
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            DataContext = App.ViewModel;
 
-            
-			// Meine SETUP Funktionen
+
+            // Meine SETUP Funktionen
             //watchPositionOfLongListSelector();
             App.ViewModel.ReloadPocalApptsAndDays();
 
-           
+            AgendaViewListbox.ManipulationStateChanged += AgendaScrolling_WhileSingleDayViewIsOpen_Fix;
 
             //SingleDayScrollViewer.SizeChanged += scrollToDefaultOffset;
 
 
-		}
+        }
 
-		#region Position im Longlistselektor bestimmen
+        private void AgendaScrolling_WhileSingleDayViewIsOpen_Fix(object sender, EventArgs e)
+        {
+            if (SingleDayView.Visibility == Visibility.Visible)
+                VisualStateManager.GoToState(this, "Close", true);
 
-		// Wird benötigt um die Position im Longlistselektor zu bestimmen um damit DeltaDays auszurechnen.
-		private Dictionary<object, ContentPresenter> items = new Dictionary<object, ContentPresenter>();
-
-		private void watchPositionOfLongListSelector()
-		{
-			AgendaViewListbox.ItemRealized += LLS_ItemRealized;
-			AgendaViewListbox.ItemUnrealized += LLS_ItemUnrealized;
-
-			DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
-			dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-			dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 40); // TODO performance
-			dispatcherTimer.Start();
-		}
-
-		private void dispatcherTimer_Tick(object sender, EventArgs e)
-		{
-
-			Day testday = (Day)GetFirstVisibleItem(AgendaViewListbox);
-			if (testday != null)
-			{
-				// Switch Highlighted Day
-				Day d = App.ViewModel.CurrentTop;
-				if (d != null)
-				{
-					d.IsHighlighted = false;
-				}
-				App.ViewModel.CurrentTop = testday;
-				testday.IsHighlighted = true;
-			}
-
-		}
-
-		public object GetFirstVisibleItem(LongListSelector lls)
-		{
-			if (items.Count > 1)
-			{
-				var offset = FindViewport(lls).Viewport.Top;
-				return (items.Where(x => Canvas.GetTop(x.Value) + x.Value.ActualHeight > offset)
-					.OrderBy(x => Canvas.GetTop(x.Value)).ToList())[1].Key;
-			}
-			else
-				return null;
-
-		}
-
-		private void LLS_ItemRealized(object sender, ItemRealizationEventArgs e)
-		{
-			if (e.ItemKind == LongListSelectorItemKind.Item)
-			{
-				object o = e.Container.DataContext;
-				items[o] = e.Container;
-			}
-		}
-
-		private void LLS_ItemUnrealized(object sender, ItemRealizationEventArgs e)
-		{
-			if (e.ItemKind == LongListSelectorItemKind.Item)
-			{
-				object o = e.Container.DataContext;
-				items.Remove(o);
-			}
-		}
-
-		private static ViewportControl FindViewport(DependencyObject parent)
-		{
-			var childCount = VisualTreeHelper.GetChildrenCount(parent);
-			for (var i = 0; i < childCount; i++)
-			{
-				var elt = VisualTreeHelper.GetChild(parent, i);
-				if (elt is ViewportControl) return (ViewportControl)elt;
-				var result = FindViewport(elt);
-				if (result != null) return result;
-			}
-			return null;
-		}
-		#endregion
-
-		#region Events
-
-		public void onDayTap(object sender, System.Windows.Input.GestureEventArgs e)
-		{
-			Debug.WriteLine("Day Tap");
-
-			//Open SingleDayView
-			SingleDayView.Visibility = Visibility.Visible;
-			VisualStateManager.GoToState(this, "OpenDelay", true);
+        }
 
 
-			//Set selectedItem 
-			var element = (FrameworkElement)sender;
-			Day selectedItem = element.DataContext as Day;
-			App.ViewModel.SingleDayViewModel.TappedDay = selectedItem;
-
-			if (apptWasTapped == false)
-			{
-				SingleDayScrollViewer.ScrollToVerticalOffset(70 * 12);
-				
-			}
-			if (firstTimeOpenSingleDayview == false)
-			{
-				 apptWasTapped = false;
-			}
-		}
-
-		private void scrollToDefaultOffset(object sender, EventArgs e)
-		{
-			if (apptWasTapped == false)
-				SingleDayScrollViewer.ScrollToVerticalOffset(70 * 12);
-			
-			firstTimeOpenSingleDayview = false;
-			
-		}
-
-		private void gridExit_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
-		{
-			closeDayView();
-		}
-
-		private void gridExit_Tap(object sender, System.Windows.Input.GestureEventArgs e)
-		{
-			closeDayView();
-		}
-
-		private void closeDayView()
-		{
-			//SingleDayView.Visibility = Visibility.Collapsed;
-			VisualStateManager.GoToState(this, "Close", true);
-		}
 
 
-		
+        #region Position im Longlistselektor bestimmen
 
-		public void ApptListItem_Tap(object sender, System.Windows.Input.GestureEventArgs e)
-		{
+        // Wird benötigt um die Position im Longlistselektor zu bestimmen um damit DeltaDays auszurechnen.
+        private Dictionary<object, ContentPresenter> items = new Dictionary<object, ContentPresenter>();
+
+        private void watchPositionOfLongListSelector()
+        {
+            AgendaViewListbox.ItemRealized += LLS_ItemRealized;
+            AgendaViewListbox.ItemUnrealized += LLS_ItemUnrealized;
+
+            DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 40); // TODO performance
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+
+            Day testday = (Day)GetFirstVisibleItem(AgendaViewListbox);
+            if (testday != null)
+            {
+                // Switch Highlighted Day
+                Day d = App.ViewModel.CurrentTop;
+                if (d != null)
+                {
+                    d.IsHighlighted = false;
+                }
+                App.ViewModel.CurrentTop = testday;
+                testday.IsHighlighted = true;
+            }
+
+        }
+
+        public object GetFirstVisibleItem(LongListSelector lls)
+        {
+            if (items.Count > 1)
+            {
+                var offset = FindViewport(lls).Viewport.Top;
+                return (items.Where(x => Canvas.GetTop(x.Value) + x.Value.ActualHeight > offset)
+                    .OrderBy(x => Canvas.GetTop(x.Value)).ToList())[1].Key;
+            }
+            else
+                return null;
+
+        }
+
+        private void LLS_ItemRealized(object sender, ItemRealizationEventArgs e)
+        {
+            if (e.ItemKind == LongListSelectorItemKind.Item)
+            {
+                object o = e.Container.DataContext;
+                items[o] = e.Container;
+            }
+        }
+
+        private void LLS_ItemUnrealized(object sender, ItemRealizationEventArgs e)
+        {
+            if (e.ItemKind == LongListSelectorItemKind.Item)
+            {
+                object o = e.Container.DataContext;
+                items.Remove(o);
+            }
+        }
+
+        private static ViewportControl FindViewport(DependencyObject parent)
+        {
+            var childCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (var i = 0; i < childCount; i++)
+            {
+                var elt = VisualTreeHelper.GetChild(parent, i);
+                if (elt is ViewportControl) return (ViewportControl)elt;
+                var result = FindViewport(elt);
+                if (result != null) return result;
+            }
+            return null;
+        }
+        #endregion
+
+        #region Events
+
+        public void onDayTap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+
+
+            //Open SingleDayView
+            SingleDayView.Visibility = Visibility.Visible;
+            VisualStateManager.GoToState(this, "OpenDelay", true);
+
+
+            //Set selectedItem 
+            var element = (FrameworkElement)sender;
+            Day selectedItem = element.DataContext as Day;
+            App.ViewModel.SingleDayViewModel.TappedDay = selectedItem;
+
+            if (apptWasTapped == false)
+            {
+                SingleDayScrollViewer.ScrollToVerticalOffset(70 * 12);
+
+            }
+            if (firstTimeOpenSingleDayview == false)
+            {
+                apptWasTapped = false;
+            }
+        }
+
+        private void scrollToDefaultOffset(object sender, EventArgs e)
+        {
+            if (apptWasTapped == false)
+                SingleDayScrollViewer.ScrollToVerticalOffset(70 * 12);
+
+            firstTimeOpenSingleDayview = false;
+
+        }
+
+        private void gridExit_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            closeDayView();
+        }
+
+        private void gridExit_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            closeDayView();
+        }
+
+        private void closeDayView()
+        {
+            //SingleDayView.Visibility = Visibility.Collapsed;
+            VisualStateManager.GoToState(this, "Close", true);
+        }
+
+
+
+
+        public void ApptListItem_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
             PocalAppointment pocalAppointment = ((FrameworkElement)sender).DataContext as PocalAppointment;
             CalendarAPI.editAppointment(pocalAppointment);
-          
-           
-		}
 
-		private void SingleDay_Hourline_Tap(object sender, System.Windows.Input.GestureEventArgs e)
-		{
+
+        }
+
+        private void SingleDay_Hourline_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
             HourLine hourLine = ((FrameworkElement)sender).DataContext as HourLine;
             DateTime starttime = App.ViewModel.SingleDayViewModel.getStarTimeFromHourline(hourLine.Text);
-			CalendarAPI.addAppointment(starttime);
-         
-		}
+            CalendarAPI.addAppointment(starttime);
+
+        }
 
 
-		#endregion
+        #endregion
 
-		private void StackPanel_Tap(object sender, System.Windows.Input.GestureEventArgs e)
-		{
-			apptWasTapped = true;
+        private void StackPanel_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            apptWasTapped = true;
 
-			PocalAppointment pocalappt = ((FrameworkElement)sender).DataContext as PocalAppointment;
-			SingleDayScrollViewer.ScrollToVerticalOffset(pocalappt.StartTime.Hour* 70 - 140);
-            
-  
+            PocalAppointment pocalappt = ((FrameworkElement)sender).DataContext as PocalAppointment;
+            SingleDayScrollViewer.ScrollToVerticalOffset(pocalappt.StartTime.Hour * 70 - 140);
 
-			
 
-		}
+
+
+
+        }
 
         private void Grid_Unloaded(object sender, RoutedEventArgs e)
         {
             PocalAppointment conmen = (sender as PocalAppointment);
             //conmen.ClearValue(FrameworkElement.DataContextProperty);
             ((FrameworkElement)sender).ClearValue(FrameworkElement.DataContextProperty);
-  
+
         }
 
 
-	}
+    }
 }
